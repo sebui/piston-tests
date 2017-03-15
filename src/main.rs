1b276b81a931bc09132ff43112f1949d49688f41
sebui <sebui@github.com>
2017-03-15 13:35:14 +0100

    WIP non optimized points links

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
 
     let mut particles: Vec<(Point2<f64>, Vector2<f64>)> = {
         let mut rng = rand::thread_rng();
-        (0..10000).map(|_| (
+        (0..1000).map(|_| (
                 Point2::new(
                     rng.gen_range(0.0, WINDOW_SIZE[0] as f64),
                     rng.gen_range(0.0, WINDOW_SIZE[1] as f64)
@@ -35,8 +35,13 @@ fn main() {
             )).collect()
     };
 
-    let square = rectangle::square(0.0, 0.0, 2.0);
+    let square = rectangle::centered_square(0.0, 0.0, 4.0);
+    let line = Line::new([0.5, 0.5, 0.5, 1.0], 1.0)
+        .shape(line::Shape::Square);
+
     let mut cursor_pos = Point2::new(0.0, 0.0);
+    let mut lines: Vec<(Point2<f64>, Point2<f64>)> = Vec::new();
+
 
     while let Some(event) = window.next() {
         match event {
@@ -46,6 +51,7 @@ fn main() {
             }
 
             Input::Update(args) => {
+                lines.clear();
                 for particle in &mut particles {
                     particle.0 += particle.1 * args.dt;
                     particle.1 *= 0.9;
@@ -76,14 +82,27 @@ fn main() {
                         particle.1.y = -particle.1.y;
                     }
                 }
+
+                for x in &particles  {
+                    for y in &particles {
+                        if nalgebra::distance(&x.0, &y.0) < 50.0 {
+                            lines.push((x.0.clone(), y.0.clone()));
+                        }
+                    }
+                }
+
             }
 
             Input::Render(_) => {
                 window.draw_2d(&event, |c, g| {
                     clear([0.92, 0.94, 0.98, 1.0], g);
 
+                    for l in &lines {
+                        line.draw([l.0.x, l.0.y, l.1.x, l.1.y], &c.draw_state, c.transform, g);
+                    }
+
                     for particle in &particles {
-                        rectangle([0.0, 0.0, 0.0, 0.5], square, c.transform.trans(particle.0.x, particle.0.y), g);
+                        rectangle([0.0, 0.0, 0.0, 1.0], square, c.transform.trans(particle.0.x, particle.0.y), g);
                     }
                 });
             }
